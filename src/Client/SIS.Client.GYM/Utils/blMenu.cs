9bba94ef486adbc6eb56b3daed7f97119da12614
Dogan Günes <dogangns.98@gmail.com>
2021-07-27 00:32:34 +0300

    .

---
+++ src/Client/SIS.Client.GYM/Utils/blMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS.Client.GYM.Utils
+{
+    public class blMenu
+    {
+        public List<Data.Menus> get_List_Menus_GYM()
+        {
+            List<Data.Menus> _List_Menus = new List<Data.Menus>();
+
+            #region GYM
+            _List_Menus.Add(new Data.Menus(10, "GYM", "Üyeler", "", "-Üye Listesi", "Üye Listesi", "", "CalendarForm", "", new int[] { 2 }));
+            _List_Menus.Add(new Data.Menus(10, "GYM", "Personel", "", "-Personel Listesi", "Personel Listesi", "", "CustomerList", "", new int[] { 2 }));
+            #endregion
+
+
+            return _List_Menus;
+        }
+    }
+}
